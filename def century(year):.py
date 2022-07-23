43fb19c1714e17cd31713c9f47ceefb707c64fc7
Jeff30 <kokou@outlook.de>
2022-07-23 21:51:30 +0200

    working for first 2 century. 2nd elif will be solving the issue.

--- def century(year):.py
+++ def century(year):.py
@@ -1,6 +1,17 @@
 def century(year):
-    check = year /10
-    print(check)
-    return
+    check = year /100
+    if(check <=1):
+        print(check)
+        return print(1)
+    elif(check > 1 and check <=2):
+        print(check)
+        print(2)
+        return 2
+    elif(check >= check and check <= check + 1  ):
+        print(check)
+        print(check + 1)
+        return
 
-century(89)
+
+ 
+century(1702)
